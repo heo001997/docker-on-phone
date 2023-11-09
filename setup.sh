bcb4787dce75d5156cc68afd1ed9d6062fbf5fd0
heo001997 <heo001997@gmail.com>
2023-11-09 20:01:46 +0700

    Update up_image setup interfaces inside

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ setup-alpine -f answerfile
 # Other Option choose default,
 # Except Erase the above disk(s) and continue? => YES
 poweroff
-./up_virtual.sh 6 6
+./up_virtual.sh 6 6144
 ./up_internet.sh
 ./up_docker.sh
 docker run -d --name my-nginx -p 8000:80 nginx
