fa530eff2ce9bebb73b13363b1d99222247e48f9
heo001997 <heo001997@gmail.com>
2023-11-19 13:05:14 +0700

    update setup virtual

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 ./download_alpine_latest.sh
-./up_image.expect 6 6144 64
+sudo expect up_image.expect 6 6 64
+
+
 ./up_internet.sh
 setup-alpine -f answerfile
 # Other Option choose default,
 # Except Erase the above disk(s) and continue? => YES
 poweroff
-./up_virtual.sh 6 6144
+./up_virtual.sh 6 6
 ./up_internet.sh
 ./up_docker.sh
 docker run -d --name my-nginx -p 8000:80 nginx
