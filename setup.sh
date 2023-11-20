cbb69a28749c4e8a3c1326d38958bee593e0df8a
heo001997 <heo001997@gmail.com>
2023-11-20 08:52:26 +0700

    update setup virtual

--- setup.sh
+++ setup.sh
@@ -4,13 +4,3 @@
 sudo expect up_image.expect 6 6 64
 sudo expect up_virtual.expect 6 6
 sudo ./start_virtual.sh 6 6
-
-./up_internet.sh
-setup-alpine -f answerfile
-# Other Option choose default,
-# Except Erase the above disk(s) and continue? => YES
-poweroff
-./up_virtual.sh 6 6
-./up_internet.sh
-./up_docker.sh
-docker run -d --name my-nginx -p 8000:80 nginx
--- start_virtual.sh
+++ start_virtual.sh
@@ -6,7 +6,12 @@ cpus=$2
 
 qemu-system-x86_64 -machine q35 -m $memory -smp cpus=$cpus -cpu qemu64 \
   -drive if=pflash,format=raw,read-only=on,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
-  -netdev user,id=n1,hostfwd=tcp::2222-:22,hostfwd=tcp::8000-:80 \
+  -netdev user,id=n1,hostfwd=tcp::10022-:22, \
+    hostfwd=tcp::10080-:80, \
+    hostfwd=tcp::18080-:8080, \
+    hostfwd=tcp::10443-:443, \
+    hostfwd=tcp::13000-:3000, \
+    hostfwd=tcp::13035-:3035, \
   -device virtio-net,netdev=n1 \
   -fsdev local,id=fsdev0,path=$PREFIX/share/virtual_mount,security_model=mapped \
   -device virtio-9p-pci,fsdev=fsdev0,mount_tag=hostshare \
